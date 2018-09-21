f072b4bb453347d2645c78f01acb4f652141aff5
Jens Verstappen <jensverstappen@hotmail.com>
2018-09-21 09:05:12 +0200

    C# programm

---
+++ MyoVisualizedApp/MyoVisualizedApp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MyoVisualizedApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataTrackBar = new System.Windows.Forms.TrackBar();
+            this.dataLoadBtn = new System.Windows.Forms.Button();
+            this.dataGraph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.startTime = new System.Windows.Forms.Timer(this.components);
+            this.btnStartSimulation = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataTrackBar
+            // 
+            this.dataTrackBar.Location = new System.Drawing.Point(12, 252);
+            this.dataTrackBar.Maximum = 80000;
+            this.dataTrackBar.Name = "dataTrackBar";
+            this.dataTrackBar.Size = new System.Drawing.Size(675, 45);
+            this.dataTrackBar.TabIndex = 0;
+            this.dataTrackBar.Value = 500;
+            // 
+            // dataLoadBtn
+            // 
+            this.dataLoadBtn.Location = new System.Drawing.Point(12, 303);
+            this.dataLoadBtn.Name = "dataLoadBtn";
+            this.dataLoadBtn.Size = new System.Drawing.Size(75, 23);
+            this.dataLoadBtn.TabIndex = 1;
+            this.dataLoadBtn.Text = "Load";
+            this.dataLoadBtn.UseVisualStyleBackColor = true;
+            this.dataLoadBtn.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // dataGraph
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.dataGraph.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.dataGraph.Legends.Add(legend6);
+            this.dataGraph.Location = new System.Drawing.Point(-1, -3);
+            this.dataGraph.Name = "dataGraph";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.dataGraph.Series.Add(series6);
+            this.dataGraph.Size = new System.Drawing.Size(701, 249);
+            this.dataGraph.TabIndex = 2;
+            this.dataGraph.Text = "chart1";
+            // 
+            // startTime
+            // 
+            this.startTime.Interval = 1;
+            this.startTime.Tick += new System.EventHandler(this.startTime_Tick);
+            // 
+            // btnStartSimulation
+            // 
+            this.btnStartSimulation.Location = new System.Drawing.Point(105, 303);
+            this.btnStartSimulation.Name = "btnStartSimulation";
+            this.btnStartSimulation.Size = new System.Drawing.Size(75, 23);
+            this.btnStartSimulation.TabIndex = 3;
+            this.btnStartSimulation.Text = "Start";
+            this.btnStartSimulation.UseVisualStyleBackColor = true;
+            this.btnStartSimulation.Click += new System.EventHandler(this.btnStartSimulation_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 338);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStartSimulation);
+            this.Controls.Add(this.dataGraph);
+            this.Controls.Add(this.dataLoadBtn);
+            this.Controls.Add(this.dataTrackBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar dataTrackBar;
+        private System.Windows.Forms.Button dataLoadBtn;
+        private System.Windows.Forms.DataVisualization.Charting.Chart dataGraph;
+        private System.Windows.Forms.Timer startTime;
+        private System.Windows.Forms.Button btnStartSimulation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MyoVisualizedApp/MyoVisualizedApp/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace MyoVisualizedApp
+{
+    public partial class Form1 : Form
+    {
+        List<Sample> runData = new List<Sample>();
+        const float upperThreshold = 120;
+        const float lowerThreshold = 120;
+        bool aboveUpperThresholdValue = false;
+
+        int stepCounter = 0;
+
+        public char SplitChar = ',';
+        public float roll, pitch, yaw, gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z;
+        int simTimer = 0;
+
+        private void startTime_Tick(object sender, EventArgs e)
+        {
+            if(simTimer < runData.Last().time)
+            {
+                simTimer += 10;
+                label1.Text = simTimer + "";
+                dataTrackBar.Value = simTimer;
+                foreach(Sample a in runData)
+                {
+                    if(a.time < simTimer + 10 && a.time > simTimer)
+                    {
+                        dataGraph.Series["Roll"].Points.AddXY(a.time, a.gyro_z);
+
+                        if (a.gyro_z > upperThreshold && !aboveUpperThresholdValue)
+                        {
+                            aboveUpperThresholdValue = true;
+                            stepCounter++;
+                        }
+                        else if (a.gyro_z < upperThreshold && aboveUpperThresholdValue)
+                        {
+                            aboveUpperThresholdValue = false;
+                            stepCounter++;
+                        }
+                    }
+                }
+                label2.Text = stepCounter + "";
+            }
+            //label1.Text = startTime.Tick();
+        }
+
+        public Int32 time;
+
+        private void btnStartSimulation_Click(object sender, EventArgs e)
+        {
+            startTime.Start();
+            dataTrackBar.Minimum = 0;
+            dataTrackBar.Maximum = runData.Last().time+10;
+            /*
+            foreach (Sample a in runData)
+            {
+
+                if(a.time.ToString() == startTime.ToString())
+                {
+                    MessageBox.Show("Test");
+                }
+            }
+            */
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }  
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            string sFileName = "";
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    sFileName = openFileDialog.FileName;
+                }
+
+                dataGraph.Series.Add("Roll");
+                dataGraph.Series["Roll"].ChartType = SeriesChartType.Line;
+                StreamReader reader = new StreamReader(sFileName);
+                while (reader.Peek() > -1)
+                {
+                    string DataLine = reader.ReadLine();
+                    if (DataLine == File.ReadAllLines(sFileName).Last())
+                    {
+                        //break;
+                    }
+                    else
+                    {
+                        string[] singleLine = DataLine.Split(SplitChar);
+                        float.TryParse(singleLine[0], out roll);
+                        float.TryParse(singleLine[1], out pitch);
+                        float.TryParse(singleLine[2], out yaw);
+                        float.TryParse(singleLine[3], out gyro_x);
+                        float.TryParse(singleLine[4], out gyro_y);
+                        float.TryParse(singleLine[5], out gyro_z);
+                        float.TryParse(singleLine[6], out accel_x);
+                        float.TryParse(singleLine[7], out accel_y);
+                        float.TryParse(singleLine[8], out accel_z);
+                        Int32.TryParse(singleLine[9], out time);
+
+                        Sample sampleData = new Sample(roll, pitch, yaw, gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, time);
+                        runData.Add(sampleData);
+                    }
+                }
+                dataGraph.Series["Roll"].ChartArea = "ChartArea1";
+                dataGraph.ChartAreas[0].AxisY.Minimum = -300;
+                dataGraph.ChartAreas[0].AxisY.Maximum = 300;
+                dataGraph.ChartAreas[0].AxisX.Minimum = 0;
+                dataGraph.ChartAreas[0].AxisX.Maximum = runData.Last().time+10;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
