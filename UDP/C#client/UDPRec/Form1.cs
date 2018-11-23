717ff80efb77c910f25940b961b77097759c479d
Jens Verstappen <jensverstappen@hotmail.com>
2018-11-23 10:57:34 +0100

    C#client2.0

---
+++ UDP/C#client/UDPRec/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPRec
+{
+    public partial class Form1 : Form
+    {
+        UdpClient Client = new UdpClient(1111); //port
+        string data = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Client.BeginReceive(new AsyncCallback(recv), null);
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += ex.Message.ToString();
+                throw;
+            }
+        }
+
+        void recv(IAsyncResult res)
+        {
+            IPEndPoint RemoteIP = new IPEndPoint(IPAddress.Any, 60240);
+            byte[] received = Client.EndReceive(res, ref RemoteIP);
+            data = Encoding.UTF8.GetString(received);
+
+            //to avoid cross-trheading we use Method Invoker
+            this.Invoke(new MethodInvoker(delegate
+            {
+                richTextBox1.Text += "\nReceived data: " + data;
+            }));
+
+            Client.BeginReceive(new AsyncCallback(recv), null);
+        }
+    }
+}
