23b1665eca1112f0c62529be82841e3335da332e
Jens Verstappen <jensverstappen@hotmail.com>
2018-11-29 16:02:31 +0100

    Working UDP client & server

---
+++ UDPV2/UDPClient/UDPClient/UDPClient.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <WS2tcpip.h>
+#include "pch.h"
+
+class UDPClient
+{
+public:
+	UDPClient();
+	~UDPClient();
+	void Write(char[] message);
+
+	
+
+private:
+	WSADATA data;
+
+	WORD version = MAKEWORD(2, 2);
+
+	sockaddr_in server;
+
+	SOCKET out;
+
+	//functies
+};
